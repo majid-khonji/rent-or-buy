50e5f8ee88b90b53a3f33c01c6c2b495ab6fdd43
Majid Khonji <majid.khonji@gmail.com>
2021-01-20 15:23:17 +0400

    RPOA

--- alg.py
+++ alg.py
@@ -19,6 +19,33 @@ def DPOA(ins: i.Instance, pred:i.Predictor, w):
             # print("keep renting..")
     return t+1
 
+# assuming delta T = 1
+def RPOA(ins: i.Instance, pred:i.Predictor, w):
+    pred.w = w
+    gamma_vals = np.arange((1-w)*ins.B + 1)
+    dis = [np.exp(g/(ins.B * (1-w)))/(ins.B *(1-w)) for g in gamma_vals]
+    # to obtain probability distribution
+    dis = dis/sum(dis)
+    for t in np.arange(ins.D):
+        # print("t = ", t)
+        val = pred.predict(t)
+        if val != None:
+            D_ = val
+            # print("D_ = ", D_)
+        else:
+            D_ = t + w*ins.B
+
+        # gen a random gamma
+        gamma = np.random.choice(gamma_vals, 1,p= dis) 
+
+        if D_ >= gamma + w*ins.B or t >= gamma+w*ins.B:
+            # print("Buy...")
+            return t + ins.B
+        # else:
+            # print("keep renting..")
+    return t+1
+
+
 def OPT(ins: i.Instance):
     if ins.D < ins.B:
         return ins.D
--- input.py
+++ input.py
@@ -2,7 +2,7 @@ import numpy as np
 class Instance:
     T = 100 # max duration
     D = None # resource duration
-    B = 60 # price to buy
+    B = 50 # price to buy
     def __init__(self, B=60):
         self.D = np.random.randint(1, self.T)
         self.B = B
@@ -15,12 +15,12 @@ class Predictor:
     def predict(self, t):
         pass
 
-class PrefectPredictor(Predictor):
+class PerfectPredictor(Predictor):
     def __init__(self, ins:Instance, w = None):
         self.ins = ins
         self.w = w if w!= None else None
     def predict(self, t):
-        if t <= self.ins.D <= t + self.w * self.ins.B >= self.ins.B:
+        if t <= self.ins.D <= t + self.w * self.ins.B: #>= self.ins.B:
             return self.ins.D
         else:
             return None
