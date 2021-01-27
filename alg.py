487a2f4a30f803c82f43cc6bcbf4fd6080f5fd70
Majid Khonji <majid.khonji@gmail.com>
2021-01-27 05:28:22 +0400

    input restructuring. OLPA implemented [but still slow]

--- alg.py
+++ alg.py
@@ -2,14 +2,13 @@ import numpy as np
 import input as i
 
 # assuming delta T = 1
-def DPOA(ins: i.Instance, pred:i.Predictor, w):
-    pred.w = w
+def DPOA(ins: i.Instance, w):
+    ins.w = w
     for t in np.arange(ins.D):
         # print("t = ", t)
-        val = pred.predict(t)
+        val = ins.predict(t)
         if val != None:
             D_ = val
-            # print("D_ = ", D_)
         else:
             D_ = t + w*ins.B
         if D_ >= ins.B or t >= ins.B:
@@ -20,24 +19,24 @@ def DPOA(ins: i.Instance, pred:i.Predictor, w):
     return t+1
 
 # assuming delta T = 1
-def RPOA(ins: i.Instance, pred:i.Predictor, w):
-    pred.w = w
+def RPOA(ins: i.Instance, w):
+    ins.w = w
 
     upper = (1-w)*ins.B
-    # upper = 1000000
     # gamma_vals = np.arange(upper)
     # dis = [np.exp(g/upper)/((np.exp(1)-1) * upper) for g in gamma_vals]
-    # to obtain probability distribution
+    # We need to normalize for some reason
     # dis = dis/sum(dis)
     # gen a random gamma
     # gamma = np.random.choice(gamma_vals, 1,p= dis)
+
+    # using CDF
     gamma = upper * np.log(np.random.uniform() * (np.exp(1) - 1) + 1)
     for t in np.arange(ins.D):
         # print("t = ", t)
-        val = pred.predict(t)
+        val = ins.predict(t)
         if val != None:
             D_ = val
-            # print("D_ = ", D_)
         else:
             D_ = t + w*ins.B
 
@@ -56,4 +55,26 @@ def OPT(ins: i.Instance):
         return ins.B
 
 
+
+# W: number of samples for w
+def OLPA(m_ins: i.MultiInstance, e=0.25, alg=DPOA, W=100):
+    K = m_ins.K
+    R = np.zeros(shape=(K+1,W))
+    w_vals = np.linspace(0, 1, W)
+    windows = [] # output windows
+    sol = 0
+    for k in np.arange(1, K+1):
+        dis = [np.exp(-e/(8*m_ins.B**2) * R[k-1, w] ) for w in np.arange(W)]
+        dis = dis/sum(dis)
+        w = np.random.choice(np.arange(W), 1,p= dis)
+        sol = sol + alg(m_ins.ins[k], w_vals[w])
+
+        windows.append(w_vals[w])
+
+        for w in np.arange(W):
+            R[k,w] = R[k-1,w] + alg(m_ins.ins[k], w_vals[w])
+
+
+    sol_min_w = np.min(R[K,:])
+    return sol/K, sol_min_w/K
 
--- input.py
+++ input.py
@@ -3,40 +3,40 @@ class Instance:
     T = 100 # max duration
     D = None # resource duration
     B = 50 # price to buy
-    def __init__(self, B=60):
+    noisy_D = 0 # read by the predictor
+    w = 0 # predictor window
+    def __init__(self, w=0.1, B=50, predictor_std = 0):
         self.D = np.random.randint(1, self.T)
         self.B = B
+        self.w = w
+        if predictor_std != 0:
+            self.noisy_D = int(np.random.normal(loc=self.D, scale=predictor_std))
+        else:
+            self.noisy_D = self.D
+
     def needed(self, t):
         return t < self.D
 
-class Predictor:
-    ins:Instance = None
-    w = .5 # prediction horizon
     def predict(self, t):
-        pass
-
-class PerfectPredictor(Predictor):
-    def __init__(self, ins:Instance, w = None):
-        self.ins = ins
-        self.w = w if w!= None else None
-    def predict(self, t):
-        if t <= self.ins.D <= t + self.w * self.ins.B: #>= self.ins.B:
-            return self.ins.D
+        if t <= self.noisy_D <= t + self.w * self.B:  # >= self.ins.B:
+            return self.noisy_D
         else:
             return None
 
+class MultiInstance:
+    B = 50
+    ins = []
+    K= None
 
-
-class NoisyPredictor(Predictor):
-    def __init__(self, ins:Instance, w = None, std = 25):
-        self.ins = ins
-        self.w = w if w!= None else None
-        self.std = std
-    def predict(self, t):
-        noisy_D  = np.random.normal(loc = self.ins.D, scale = self.std)
-        # print("noisy D: ", noisy_D)
-        if t <= noisy_D <= t + self.w\
-                * self.ins.B:
-            return self.ins.D
+    # K: overrides the upper bound of k in theorem 3, None will take the default value
+    def __init__(self, K=10, B=50, predictor_std=0):
+        self.B = B
+        if K == None:
+            theta = 2 * np.log(12 * self.B ** 2 / np.exp(1))
+            self.K = int(np.ceil(12 * self.B ** 2 * theta / np.exp(2)))
         else:
-            return None
+            self.K = K
+
+        self.ins = [Instance(B=self.B, predictor_std=predictor_std) for _ in np.arange(self.K + 1)]
+
+
