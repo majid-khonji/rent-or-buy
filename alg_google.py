02ba147920d52bb09c57452edadfbac5ebeb8938
zhouyue <zhouyue360@gmail.com>
2021-02-02 14:06:18 +1100

    fixed google algorithm

--- alg_google.py
+++ alg_google.py
@@ -5,7 +5,6 @@ import input as i
 def DPOA_google(ins: i.Instance, w):
     ins.w = w
     _lambda = 1 - w
-
     D_ = ins.noisy_D
     if D_ >= ins.B:
         if ins.D < math.ceil(ins.B * _lambda):
@@ -22,27 +21,32 @@ def DPOA_google(ins: i.Instance, w):
 def RPOA_google(ins: i.Instance, w):
     ins.w = w
     _lambda = 1 - w
+    if _lambda < 1 / ins.B:
+        _lambda = 1 / ins.B
     D_ = ins.noisy_D
     if D_ >= ins.B:
         k = math.floor(_lambda * ins.B)
+        k = max(1,k)
         i_vals = np.arange(k)
         dis = [math.pow((ins.B - 1) / ins.B, k - m) / (ins.B * (1 - math.pow(1 - 1 / ins.B, k))) for m in i_vals]
         _sum = sum(dis)
         _dis = [x / _sum for x in dis]
         j = np.random.choice(i_vals, 1, p=_dis)
+        j = max(1,j)
         if ins.D < j:
             return ins.D
         else:
             return j + ins.B - 1
-
-        return j + ins.B - 1
     else:
         l = math.ceil(ins.B / _lambda)
+        l = max(1,l)
         i_vals = np.arange(l)
         dis = [math.pow((ins.B - 1) / ins.B, l - m) / (ins.B * (1 - math.pow(1 - 1 / ins.B, l))) for m in i_vals]
         _sum = sum(dis)
         _dis = [x / _sum for x in dis]
         j = np.random.choice(i_vals, 1, p=_dis)
+        j = max(1,j)
+
         if ins.D < j:
             return ins.D
         else:
